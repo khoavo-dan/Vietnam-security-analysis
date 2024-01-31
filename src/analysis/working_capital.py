5869e4d6641ed0ea6c1c7424daed337ae293c620
khoavo-dan <dangkhoa.vo@proton.me>
2024-01-31 20:37:23 +0700

    Fix net-net working capital module due to mistaking dataframe processing

--- src/analysis/working_capital.py
+++ src/analysis/working_capital.py
@@ -1,7 +1,7 @@
 import numpy as np
 from src.analysis import basis
 
-def quarterly_ncav(report_data, ticker, share_outstanding, price):
+def quarterly_ncav(report_data, ticker, share_outstanding):
     # cache = {}
     # for i in ['balancesheet','incomestatement']:
     #     for j in report_data[f'{ticker}_{i}'].index:
@@ -10,24 +10,15 @@ def quarterly_ncav(report_data, ticker, share_outstanding, price):
 
         current_asset = report_data[f'{ticker}_balancesheet'].loc['CURRENT ASSETS']
         cash = report_data[f'{ticker}_balancesheet'].loc['Cash and cash equivalents']
-        short_term_investments = report_data[f'{ticker}_balancesheet'].loc['Short-term investments']
+        short_term_investments = report_data[f'{ticker}_balancesheet'].loc['Short-term investments'].iloc[0]
         receivables = report_data[f'{ticker}_balancesheet'].loc['Accounts receivable']
         inventory = report_data[f'{ticker}_balancesheet'].loc['Inventories'].iloc[0]
         liabilities = report_data[f'{ticker}_balancesheet'].loc['LIABILITIES']
 
         net_current_asset_value = current_asset - liabilities
-        net_net_working_capital = cash + short_term_investments + receivables*0.75 + inventory*0.5 - liabilities
-        ncav = net_current_asset_value[-1]/share_outstanding[ticker]
-        nnwc = net_net_working_capital[-1]/share_outstanding[ticker]
+        net_net_working_capital = (cash + short_term_investments + receivables*0.75 + inventory*0.5 - liabilities)/share_outstanding[ticker]
 
     except Exception as e:
         print(e)
-    # if price != 0:
-    #     print(f'{ticker} \n    Current price: {price}')
-    #     print(f'    Net curent asset value per share (ncav): {ncav:,.2f}')
-    #     print(f'    Net-net working capital per share (nnwc): {nnwc:,.2f}        P/L margin: {(nnwc/price[ticker]-1)*100:.2f}%')
-    # else:
-    #     print(f'    Net curent asset value per share (ncav): {ncav:,.2f}')
-    #     print(f'    Net-net working capital per share (nnwc): {nnwc:,.2f}')
 
-    return ncav, nnwc
+    return net_net_working_capital[-1]
